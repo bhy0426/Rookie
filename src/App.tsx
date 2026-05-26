375c6f9f54e188a4661d948c17a148aa0defa753
Hayeon Byun <bhy246@naver.com>
2026-05-27 00:08:34 +0900

    2026_05_27_install_bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,20 @@ import './App.css'
 function App() {
   return (
     <>
+    <ul className="nav">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
       <h1>저희는 숨을 쉬면서 살아갑니다</h1>
     </>
   )
