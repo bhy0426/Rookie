586acea9a058592d72ef4241807514788e43a9c9
Hayeon Byun <bhy246@naver.com>
2026-06-02 22:53:05 +0900

    2026_06_02

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import '../App.css'
+
+const About: React.FC = () => {
+    return (
+        <>
+            <div className="d-flex flex-column mb-3">
+                <div className="p-2">Flex item 1</div>
+                <div className="p-2">Flex item 2</div>
+                <div className="p-2">Flex item 3</div>
+            </div>
+        </>
+    )
+};
+
+export default About;
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,22 +2,29 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../App.css'
+import About from './About';
+import { Route, Routes } from 'react-router-dom';
 
 // 2단계: 부품 틀 만들기 (const)
 const NavigationBar: React.FC = () => {
 
   // 3단계: 화면 그리기 (return)
   return (
+    <>
       <Navbar className="customNav">
-        <Container>
-          <Navbar.Brand href="#home">숨</Navbar.Brand>
-          <Nav className="justify-content-md-center">
-            <Nav.Link href="#home">숨에 관하여</Nav.Link>
-            <Nav.Link href="#features">사용</Nav.Link>
-            <Nav.Link href="#pricing">연결</Nav.Link>
+        <Container className="d-flex justify-content-center">
+          <Navbar.Brand href="/">숨</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/about">숨에 관하여</Nav.Link>
+            <Nav.Link href="/use">사용</Nav.Link>
+            <Nav.Link href="/connect">연결</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </>
   )
 }; 
 
