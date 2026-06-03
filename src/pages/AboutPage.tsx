b7a5b49b19a06202a9ce2ace57c3b65eeea94ca8
Hayeon Byun <bhy246@naver.com>
2026-06-03 22:22:15 +0900

    2026_06_03_2

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,1 +1,9 @@
-export default function AboutPage(){ return <h1>About Page</h1>; }
+const AboutPage: React.FC = () => {
+    return(
+        <>
+            여기는 숨에 관하여 페이지입니다.
+        </>
+    )
+};
+
+export default AboutPage;
