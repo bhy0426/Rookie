9db086cc487af732c8be5122e80da050482d4941
Rabbit <hokpari030224@bible.ac.kr>
2026-06-07 02:33:02 +0900

    20260607_footer/navbar_Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
 import icon from "../pic/icon.png";
 
+const handleFooterClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+  event.preventDefault();
+
+  if (window.location.pathname === "/") {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  } else {
+    window.location.href = "/";
+  }
+};
+
 const Footer = () => (
   <footer>
   <div className="footer-container">
-    <a href="#main">
-      <img src={icon} className="footer-img"></img>
+    <a href="#main" onClick={handleFooterClick}>
+      <img src={icon} className="footer-img"></img> {/*부트스트랩 전역설정 때문에 부드럽게 움직이는거*/}
     </a>
     {/* <img src={icon} className="footer-img"></img> */}
     <p className="footer-text">© 2026 Rookie. All rights reserved.</p>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useState } from "react";
+//import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../App.css';
 
+
 const NavigationBar: React.FC = () => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false); //useState를 사용해서 네비게이션 바의 확장 여부를 관리
+
   return (
-    <Navbar className="customNav" id="main">
-      <Container className="d-flex justify-content-center">
-        <Navbar.Brand href="/">숨</Navbar.Brand>
-        <Nav>
-          <Nav.Link href="/about">숨에 관하여</Nav.Link>
-          <Nav.Link href="/usage">사용</Nav.Link>
-          <Nav.Link href="/connect">연결</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <nav
+      className={`navbar ${isExpanded ? "expanded" : ""}`}
+      onMouseEnter={() => setIsExpanded(true)}  //마우스가 네비게이션 바에 들어오면 확장 true
+      onMouseLeave={() => setIsExpanded(false)} //마우스가 네비게이션 바에서 나가면 확장 false
+    >
+      <div className="nav-main">  
+        <a href="/">숨</a>
+        <a href="/about">숨에 관하여</a>
+        <a href="/usage">사용</a>
+        <a href="/connect">연결</a>
+      </div>
+
+      <div className={`nav-panel ${isExpanded ? "expanded" : ""}`}>
+        <a href="/about">다른거임 ㅇㅇ</a>
+        <a href="/usage">사ㅁㄴㅇㄹ</a>
+        <a href="/connect">연ㅁㄴㅇㄹ</a>
+      </div>
+    </nav>
   );
+
+  // return (
+  //   <Navbar className="customNav" id="main">
+  //     <Container className="d-flex justify-content-center">
+  //       <Navbar.Brand href="/">숨</Navbar.Brand>
+  //       <Nav>
+  //         <Nav.Link href="/about">숨에 관하여</Nav.Link>
+  //         <Nav.Link href="/usage">사용</Nav.Link>
+  //         <Nav.Link href="/connect">연결</Nav.Link>
+  //       </Nav>
+  //     </Container>
+  //   </Navbar>
+  // );
 };
 
 export default NavigationBar;
