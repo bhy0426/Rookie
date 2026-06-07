c2b9d90a42c7037bac2acf07c940de38bdfbbcd0
Rabbit <hokpari030224@bible.ac.kr>
2026-06-07 23:39:43 +0900

    20260607_Update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,11 +3,32 @@ import { useState } from "react";
 import { Container, Row, Col, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../App.css';
-import nav_panel_img from "../pic/Rectangle 3.svg";
+//import nav_panel_img from "../pic/Rectangle 3.svg";
 
+const navItems = [
+  {
+    label: "숨에 관하여",
+    path: "/about",
+    image: "/images/about-preview.png",
+    description: "숨 프로젝트의 목적과 방향을 소개합니다.",
+  },
+  {
+    label: "사용",
+    path: "/usage",
+    image: "/images/usage-preview.png",
+    description: "프로그램 사용법과 구성 방법을 확인할 수 있습니다.",
+  },
+  {
+    label: "연결",
+    path: "/connect",
+    image: "/images/connect-preview.png",
+    description: "커뮤니티와 외부 채널로 연결됩니다.",
+  },
+];
 
 const NavigationBar: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false); //useState를 사용해서 네비게이션 바의 확장 여부를 관리
+  const [hoveredItem, setHoveredItem] = useState(navItems[0]); //useState를 사용해서 텍스트 위에 마우스 여부를 관리
 
   return (
     <nav
@@ -47,7 +68,29 @@ const NavigationBar: React.FC = () => {
       </Container>
 
       <div className="nav-panel">
-        <Container fluid>
+        <div className="nav-preview">
+    <img
+      src={hoveredItem.image}
+      alt={hoveredItem.label}
+      className="nav-preview-img"
+    />
+    <p>{hoveredItem.description}</p>
+  </div>
+
+  <div className="nav-menu-list">
+    {
+      navItems.map((item) => (
+      <Link
+        key={item.path}
+        to={item.path}
+        className="nav-menu-link"
+        onMouseEnter={() => setHoveredItem(item)}
+      >
+        {item.label}
+      </Link>
+    ))}
+  </div>
+        {/* <Container fluid>
           <Row>
             <Col>
               <img src={nav_panel_img} className="nav-panel-img"></img>
@@ -105,7 +148,7 @@ const NavigationBar: React.FC = () => {
               </Row>
             </Col>
           </Row>
-        </Container>
+        </Container> */}
       </div>
     </nav>
   );
