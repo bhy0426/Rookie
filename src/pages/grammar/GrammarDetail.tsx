1ab9131e065c47d9d18dd41c1a98489ec5b3b14e
Hayeon Byun <bhy246@naver.com>
2026-06-16 02:30:51 +0900

    deploy_ffinal

--- src/pages/grammar/GrammarDetail.tsx
+++ src/pages/grammar/GrammarDetail.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import EditorBlock from './EditorBlock';
-import { grammarData } from '../../data/GrammarData';
+import { grammarData } from '../../data/grammarData';
 import '../../styles/subpages/grammar/GrammarDetail.css';
 
 export default function GrammarDetail() {
